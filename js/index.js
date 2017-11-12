867a00801db8f8a7a5e5bef77d678fb402327305
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-11-12 13:09:55 -0500

    Adjustments to landing page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,30 @@
+function QueryWatson(qry) {
+	var baseUrl = "https://gearup.mybluemix.net/news?";
+	var queryParam = "qry=";
+	var query = qry;
+	var queryUrl = baseUrl + queryParam + query;
+
+	jQuery.ajaxPrefilter(function(options) {
+		if (options.crossDomain && jQuery.support.cors) {
+			options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+		}
+	});
+
+	$.getJSON(queryUrl, function(result){
+	data = result;
+		
+	$("#myUl").empty();
+	var items = [];
+	$.each(result, function(i, field){
+		 items.push("<li>" + field.title +"</li>"+"<hr class='nspace'>");
+	});
+	 $("#myUl").append(items.join(''));
+	});
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
+
+		QueryWatson("hurricane")
 
 		$('#mainNav').affix({
 			offset: {
@@ -8,30 +34,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		})
 
 		document.getElementById('shelter').addEventListener("click", function( innerEvent ) {
-					var baseUrl = "https://gearup.mybluemix.net/news?";
-					var queryParam = "qry=";
-					var query = $("#newsQuery").val();
-					var queryUrl = baseUrl + queryParam + query;
-
-					jQuery.ajaxPrefilter(function(options) {
-						if (options.crossDomain && jQuery.support.cors) {
-							options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-						}
-					});
-
-					$.getJSON(queryUrl, function(result){
-					data = result;
-
-					// console.log(queryUrl);
-
-					$("#myUl").empty();
-					var items = [];
-					$.each(result, function(i, field){
-						 items.push("<li>" + field.title +"</li>"+"<hr class='nspace'>");
-					});
-					 $("#myUl").append(items.join(''));
-					});
-		 });
+			QueryWatson($("#newsQuery").val());
+		});
 
 		$(document).on('click', 'a[href^="#"]', function () {
 				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
