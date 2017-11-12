9a1469cfa5f92dc287a770fbefa53cba8539df7f
Navidur Rahman <navid.rvl@gmail.com>
2017-11-12 03:18:08 -0500

    update news ui and cleanup

--- js/index.js
+++ js/index.js
@@ -1,59 +1,49 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	
+
 		$('#mainNav').affix({
 			offset: {
 				top: 100,
 			}
-	
+
 		})
-	
+
 		document.getElementById('shelter').addEventListener("click", function( innerEvent ) {
 					var baseUrl = "https://gearup.mybluemix.net/news?";
 					var queryParam = "qry=";
 					var query = $("#newsQuery").val();
 					var queryUrl = baseUrl + queryParam + query;
-	
+
 					jQuery.ajaxPrefilter(function(options) {
 						if (options.crossDomain && jQuery.support.cors) {
 							options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
 						}
 					});
-	
+
 					$.getJSON(queryUrl, function(result){
 					data = result;
-					
-					console.log(queryUrl);
-					
+
+					// console.log(queryUrl);
+
 					$("#myUl").empty();
 					var items = [];
 					$.each(result, function(i, field){
-						 items.push("<li>" + field.title +"</li>");
+						 items.push("<li>" + field.title +"</li>"+"<hr class='nspace'>");
 					});
 					 $("#myUl").append(items.join(''));
 					});
 		 });
-	
+
 		$(document).on('click', 'a[href^="#"]', function () {
-				
 				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-					
 					var $target = $(this.hash);
-					
 					$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-					
 					if ($target.length) {
-						
 						var targetOffset = $target.offset().top -0;
-						
 						$('html,body').animate({scrollTop: targetOffset}, 300);
-						
 						return false;
-						
 					}
-					
 				}
-				
 		});
-		
-	
+
+
 	})
